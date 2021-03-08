ee2fb197f5943f4639b6dc8323db9026857fffb9
daluxer-demiurge <0604411@gmail.com>
2021-03-08 20:13:00 +0400

    Практическое задание к уроку 1 по курсу Основы языка C#

---
+++ lesson1/Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ЗАДАНИЕ 3
+            //а) Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2,y2 по формуле r=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2). Вывести результат, используя спецификатор формата .2f (с двумя знаками после запятой);
+            //б) *Выполните предыдущее задание, оформив вычисления расстояния между точками в виде метода; 
+            //Сержов Михаил
+
+
+            Console.WriteLine("Введите координаты по оси Х для первой точки");
+                double x1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты по оси Y для первой точки");
+                double y1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты по оси Х для второй точки");
+                double x2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты по оси Y для второй точки");
+                double y2 = double.Parse(Console.ReadLine());
+
+
+            double r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            
+            Console.WriteLine($"Расстояние между точками с координатами x1, y1 и x2,y2: {r:F2}");
+
+ 
+
+
+        }
+    }
+}
---
+++ lesson1/lesson1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ЗАДАНИЕ 1
+            //Написать программу «Анкета». Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес). В результате вся информация выводится в одну строчку.
+            //а) используя склеивание;
+            //б) используя форматированный вывод;
+            //в) *используя вывод со знаком $.
+            //Сержов Михаил
+
+
+            Console.WriteLine ("Введите фамилию: ");
+                string sname = Console.ReadLine();
+
+            Console.WriteLine("Введите имя: ");
+                string name = Console.ReadLine();
+
+            Console.WriteLine("Введите возраст: ");
+                int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите рост: ");
+                int hight = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите вес: ");
+                int wight = int.Parse(Console.ReadLine());
+
+            //решение а)
+            Console.WriteLine(String.Format(sname + " " + name + " " + age + " " + hight + " " + wight));
+
+            //решение б)
+            Console.WriteLine(String.Format("{0} {1} {2} {3} {4}", sname, name, age, hight, wight));
+
+            //решение в)
+            Console.WriteLine($"{sname} {name} {age} {hight} {wight}");
+
+            
+
+        }
+    }
+}
