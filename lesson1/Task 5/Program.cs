ece92e23edc40afaaef5f35dad9e0723bb773338
daluxer-demiurge <0604411@gmail.com>
2021-03-08 20:11:08 +0400

    Практическое задание к уроку 1 по курсу Основы языка C#

---
+++ lesson1/Task 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ЗАДАНИЕ 5
+            //а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+            //б) Сделать задание, только вывод организуйте в центре экрана
+            //в) *Сделать задание б с использованием собственных методов(например, Print(string ms, int x, int y)
+            //Сержов Михаил
+
+            //Решение а)
+            Console.WriteLine("Михаил Сержов, Самара");
+            System.Threading.Thread.Sleep(4000);
+
+            //Решение б)
+            Console.WriteLine("А теперь тоже самое, но по центру экрана!");
+            System.Threading.Thread.Sleep(4000);
+            Console.Clear();
+           
+            string text = "Михаил Сержов, Самара";
+            int coordX = (Console.WindowWidth / 2) - (text.Length / 2);
+            int coordY = (Console.WindowHeight / 2) - 1;
+            Console.SetCursorPosition(coordX, coordY);
+            Console.WriteLine(text);
+            System.Threading.Thread.Sleep(4000);
+
+
+        }
+    }
+}
